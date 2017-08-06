0f3388ff1391016cbfef1450c8ab5f655a3a6892
itprosteer <Y.Kalishchuk@prosteergroup.com>
2017-08-06 05:18:15 +0300

    add Apllication model? migration and controller

---
+++ database/migrations/2017_08_06_012752_create_applications_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateApplicationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('applications', function (Blueprint $table) {
+            $table->increments('id');
+            $table->text('message');
+            $table->integer('reason_id')->unsigned();
+            $table->timestamp('created_at')->default(\DB::raw('CURRENT_TIMESTAMP'));
+            $table->datetime('sent_date');
+            $table->boolean('sent')->default(false);
+            
+            $table->foreign('reason_id')->references('id')->on('reasons');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('applications');
+    }
+}
